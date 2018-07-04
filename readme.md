2760fdfd6eb5f8d5f031c534644a49186301e69d
PeerHenry <peerhenry@gmail.com>
2018-07-04 11:53:45 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+#vue-boilerplate
+This project provides a minimal setup of Vue with a modular Webpack configuration as an alternative to using the vue-cli.
---
+++ src/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<script>
+export default {
+  name: '',
+  data: function(){
+    return {
+      title: 'Ahoy there'
+    }
+  },
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+  h1{
+    color: red
+  }
+</style>
